9fd0acf7017c6cca6afb6f365eef116d2a79c162
Vaishnavi <vaishnavishahakar1@gmail.com>
2025-03-14 22:12:14 +0530

    created controllers folder and user models

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    unique: true,
+  },
+  address: {
+    type: String,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, {
+    timestamps: true,
+});
+
+const User = model("User", userSchema);
+
+export default User;
