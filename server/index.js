7ac8b462800178deb75f164f1bd5c31ded9b406b
Vaishnavi <vaishnavishahakar1@gmail.com>
2025-03-16 22:07:15 +0530

    Added user signup endpoint with basic validation and error handling

--- server/index.js
+++ server/index.js
@@ -4,35 +4,92 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+import User from "./models/User.js";
+
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 //connect to mongoDB
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+  const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    if (conn) {
-        console.log(`MongoDB connected successfully`);
-    }
+  if (conn) {
+    console.log(`MongoDB connected successfully`);
+  }
 };
 
 app.get("/health", (req, res) => {
-    res.json({
-        success: true,
-        message: "Server is running",
+  res.json({
+    success: true,
+    message: "Server is running",
+  });
+});
+
+app.post("/signup", async (req, res) => {
+  const { name, email, phone, address, password, rePassword } = req.body;
+
+  if (password !== rePassword) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Passwords does not match" });
+  }
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Name is required" });
+  }
+
+  if (!email) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Email is required" });
+  }
+
+  if (!phone) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Phone is required" });
+  }
+
+  if (!address) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Address is required" });
+  }
+
+  try{
+    const newUser = new User({
+        name,
+        email,
+        phone, 
+        address,
+        password,
     });
+
+    const savedUser = await newUser.save();
+
+    return res.json({
+        success: true,
+        message: "Signup successful",
+        data: savedUser,
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
 });
 
 app.use("*", (req, res) => {
-    res.status(404).json({
-        success: true,
-        message: "API endpoint doesn't exist",
-    });
+  res.status(404).json({
+    success: true,
+    message: "API endpoint doesn't exist",
+  });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-}); 
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
