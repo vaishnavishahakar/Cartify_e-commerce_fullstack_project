3fd5745c7486b9d607ca3cf2b397e7a0ece05c6d
Vaishnavi <vaishnavishahakar1@gmail.com>
2025-03-16 19:23:40 +0530

    created payment model schema

---
+++ server/models/Payment.js
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+
+const paymentSchema = new Schema(
+  {
+    paymentMode: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    transactionId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Payment = model("Payment", paymentSchema);
+
+export default Payment;
