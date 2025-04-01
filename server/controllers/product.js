e5538d7afa0144e6f63ebff98fa2a0842c071c25
Vaishnavi <vaishnavishahakar1@gmail.com>
2025-04-01 21:16:24 +0530

    Add product posting API with mandatory field validation

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,5 +1,25 @@
 const postProducts = async (req, res) =>{
+ const {
+    name, 
+    shortDescription, 
+    longDescription, 
+    price, 
+    currentPrice, 
+    category, 
+    images, 
+    tags
+ } = req.body;
 
-}
+ const mandatoryFields = ["name", "shortDescription", "longDescription", "price", "category", "images"];
+
+ for(const field of mandatoryFields) {
+    if(!req.body[field]) {
+        return res.status(400).json({
+            success: false,
+            message: `${field} is required`
+        });
+    }
+ }
+};
 
 export { postProducts };
--- server/models/Product.js
+++ server/models/Product.js
@@ -35,6 +35,7 @@ const productSchema = new Schema({
     timestamps: true,
 });
 
+
 const Product = model("Product", productSchema);
 
 export default Product;
