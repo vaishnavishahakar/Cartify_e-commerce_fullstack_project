8ecc5df56243673ca40a1aedf0669245a3c47fe9
Vaishnavi <vaishnavishahakar1@gmail.com>
2025-05-10 17:14:27 +0530

    client initial setup

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<>
+  <h1 className='text-red-500'>E-Commerce</h1>
+</>
+ 
+);
+
