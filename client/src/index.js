59d7209c1dcc09d9132a9e6a3eb512f969a1d949
Vaishnavi <vaishnavishahakar1@gmail.com>
2025-05-10 21:26:14 +0530

    Icons setup using lucide react

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Bluetooth as BluetoothIcon, Home as HomeIcon } from 'lucide-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <>
   <h1 className='text-red-500'>E-Commerce</h1>
+  <HomeIcon size="60" />
+  
+  <BluetoothIcon size={100} />
 </>
  
 );
