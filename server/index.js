5cd5ed5ed7183d1cbec3bb8a138e147d9acb7088
Vaishnavi <vaishnavishahakar1@gmail.com>
2025-05-17 17:36:55 +0530

    add cors configuration

--- server/index.js
+++ server/index.js
@@ -35,12 +35,13 @@ app.use(express.json());
 //     credentials: true,
 //   })
 // );
-app.use(
-  cors({
-    origin: "*", // Allows all origins temporarily
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://rtc-e-commerce-project.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 
 app.use(
